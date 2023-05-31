b537614b7cd51ecc8f1b078e107270c764922b19
Anthony Sutton <anthony.sutton95@gmail.com>
2023-05-31 12:48:48 -0700

    Fixed bug for LSP
    Signed-off-by: Anthony Sutton <anthony.sutton95@gmail.com>

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -4,17 +4,17 @@ local lsp = require("lsp-zero")
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-        'tsserver',
-        'eslint',
-        'lua_ls',
-        'cssls',
-        'gopls',
-        'html',
-        'pylsp',
-        'jsonls',
-        'texlab',
-        'rust_analyzer',
-        'tailwindcss',
+    'tsserver',
+    'eslint',
+    'lua_ls',
+    'cssls',
+    'gopls',
+    'html',
+    'pylsp',
+    'jsonls',
+    'texlab',
+    'rust_analyzer',
+    'tailwindcss',
 })
 
 local cmp = require('cmp')
@@ -22,18 +22,19 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 
 cmp.setup({
-        mapping = {
-                ['<CR>'] = cmp.mapping.confirm({ select = false }),
-        }
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    }
 })
 
 
 lsp.on_attach(function(client, bufnr)
-        local opts = { buffer = bufnr, remap = false }
+    local opts = { buffer = bufnr, remap = false }
 
-        vim.keymap.set("n", "gd", function() vim.lsp.vuf.definition() end, opts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "<leader>sd", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>sd", vim.diagnostic.open_float, opts)
 end)
 
 lsp.setup()
